a500294858541afea4c6dcf1592d3863bee382de
jzheng10 <48421695+Jizh10@users.noreply.github.com>
2021-12-08 21:18:10 +0000

    auto mode

---
+++ Linear.py
@@ -0,0 +1,23 @@
+import odrive
+import time
+import numpy as np
+
+class Linear():
+
+  def __init__(self):
+      self.pulley_diameter = 12.5 #mm
+      self.dvr = odrive.find_any()
+      self.dvr.axis1.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
+      
+      while self.dvr.axis1.current_state != AXIS_STATE_IDLE:
+        time.sleep(0.1)
+
+      self.dvr.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+
+      
+  def move(self, pos, speed = 40): #pos in mm, #speed in rot/s
+    self.dvr.axis1.controller.config.vel_limit = speed
+    self.dvr.axis1.controller.input_pos = pos/(np.PI*self.pulley_diameter)
+  
+  def home(self):
+    self.dvr.axis1.requested_state = AXIS_STATE_HOMING
---
+++ rotational.py
@@ -0,0 +1,23 @@
+import RPi.GPIO as GPIO
+import time
+
+#step = 19, dir = 26
+
+class rot():
+
+  def __init__(self, step, dir):
+    self.step, self.dir = step, dir
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(self.step, GPIO.OUT, initial=0)
+    GPIO.setup(self.dir, GPIO.OUT, initial=0)
+
+  def home(self, val = 0):
+    self.cur_angle = val
+
+  def angle(self, angle, speed = 5): #speed in rot/sec
+    steps = abs(angle - self.val)/200
+    for i in range(steps):
+            GPIO.output(19,1)
+            time.sleep(1/(speed*200))
+            GPIO.output(19,0)
+            time.sleep(1/(speed*200))
