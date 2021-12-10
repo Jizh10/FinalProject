c5d58ef5a7a9ba889f3be1959cf45ca502788376
jzheng10 <48421695+Jizh10@users.noreply.github.com>
2021-12-10 20:40:03 +0000

    html

--- trig.py
+++ trig.py
@@ -1,8 +1,9 @@
 import Linear.py
 import rotational.py
+import numpy as np
 
 class trig():
   def __init__(self):
     
-  def pointcammera(x,y):
-      
+  def pointcammera(x0,y0,xc):
+      theta = 
