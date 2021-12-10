0a07050801a741de4178ff68320d9b5a47248104
jzheng10 <48421695+Jizh10@users.noreply.github.com>
2021-12-10 21:48:56 +0000

    local host

--- final_project.py
+++ final_project.py
@@ -133,7 +133,7 @@ body {font-family: Arial;}
 <body>
 
 <center>
-<img src="/www/var/html/{{imageIndex}}.jpg" alt="test image" width="500" height="333">
+<img src="http://192.168.0.210/{{imageIndex}}.jpg" alt="test image" width="500" height="333">
 <br>
 <form action="/cgi-bin/final_project.py" method="POST">
   <input type = "submit" name = "image" value = "prev image">
@@ -153,7 +153,7 @@ body {font-family: Arial;}
 <div id="normal" class="tabcontent">
   <p>Take an Image</p>
   <form action="/cgi-bin/final_project.py" method="POST">
-    <input type = "image" src = "/usr/html/camera.jpg" alt = "Submit" width = "100" height = "100">
+    <input type = "image" src = "http://192.168.0.210/camera.jpg" alt = "Submit" width = "100" height = "100">
     <input type = "hidden" name = "take image" value = "1">
     <input type = "hidden" name = "image index" value = {{imageIndex}}>
     <input type = "hidden" name = "last image index" value = {{lastImageIndex}}> 
@@ -193,7 +193,7 @@ body {font-family: Arial;}
 <div id="step" class="tabcontent">
   <p>Take an Image</p>
   <form action="/cgi-bin/final_project.py" method="POST">
-    <input type = "image" src = "/usr/html/camera.jpg" alt = "Submit" width = "100" height = "100">
+    <input type = "image" src = "http://192.168.0.210/camera.jpg" alt = "Submit" width = "100" height = "100">
     <input type = "hidden" name = "image index" value = {{imageIndex}}>
     <input type = "hidden" name = "last image index" value = {lastImageIndex}}> 
   </form>
@@ -223,7 +223,7 @@ body {font-family: Arial;}
 <div id="position" class="tabcontent">
   <p>Take an Image</p>
   <form action="/cgi-bin/final_project.py" method="POST">
-    <input type = "image" src = "/usr/FinalProject/camera.jpg" alt = "Submit" width = "100" height = "100">
+    <input type = "image" src = "http://192.168.0.210/camera.jpg" alt = "Submit" width = "100" height = "100">
     <input type = "hidden" name = "image index" value = {{imageIndex}}>
     <input type = "hidden" name = "last image index" value = {lastImageIndex}}> 
   </form>
