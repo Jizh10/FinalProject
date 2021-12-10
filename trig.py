a83c75f5d046231cb419168d1d6dd220cf678c24
jzheng10 <48421695+Jizh10@users.noreply.github.com>
2021-12-10 20:54:58 +0000

    dir chn

--- trig.py
+++ trig.py
@@ -2,12 +2,15 @@ import Linear.py
 import rotational.py
 import numpy as np
 
+stepper = rot(step = 19, dir = 26)
+
+
 class trig():
   def __init__(self):
-    
+    pass
   def pointcammera(theta0,d,xc):
       x0 = d*np.cos(theta0)
       y0 = d*np.sin(theta0)
 
       theta = np.arctan(yi/(xi-xc))
-      ro
+      stepper.move(theta)
