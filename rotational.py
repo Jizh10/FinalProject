7673585e87baf7eb172f7c24357457299ea000ff
James Cobau <jamescobau@gmail.com>
2021-12-11 00:59:26 +0000

    stepper bug

--- rotational.py
+++ rotational.py
@@ -16,14 +16,14 @@ class rot():
     self.cur_angle = val
 
   def angle(self, angle, speed = 5): #speed in rot/sec
-    steps = abs(angle - self.cur_angle)/(200*2*np.pi)
+    steps = 200*abs(angle - self.cur_angle)/(2*np.pi)
     print(steps)
     if angle - self.cur_angle < 0:
       GPIO.output(self.dir,0)
     else:
       GPIO.output(self.dir,1)
     
-    for i in range(1000):#int(steps)):
+    for i in range(int(steps)):
             GPIO.output(self.step,1)
             time.sleep(1/(speed*200))
             GPIO.output(self.step,0)
