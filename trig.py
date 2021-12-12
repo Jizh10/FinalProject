4f25e677252088ffd8d8ba9260f344ffffbae56e
James Cobau <jamescobau@gmail.com>
2021-12-12 22:22:20 +0000

    trig

--- trig.py
+++ trig.py
@@ -9,8 +9,8 @@ class trig():
   def __init__(self):
     pass
   def pointcammera(self,theta0,d,xc):
-      x0 = d*np.cos(theta0)
-      y0 = d*np.sin(theta0)
+      x0 = d*np.sin(theta0)
+      y0 = d*np.cos(theta0)
 
       theta = np.arctan(y0/(x0-xc))
       print("theta {:f}".format(theta))
