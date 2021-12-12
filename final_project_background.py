b83bed378ae99ed4a300faa9fdc63cb2bbd531bb
jzheng10 <48421695+Jizh10@users.noreply.github.com>
2021-12-12 20:26:46 +0000

    image test

--- final_project_background.py
+++ final_project_background.py
@@ -8,8 +8,11 @@ try:
   while True:
     with open("/usr/lib/cgi-bin/final_project.txt",'r+') as f:
       data = json.load(f)
+      print('data loaded')
       if data['takeImage'] == '1':
+        print('command received')
         camera.capture('/www/var/html/image.jpg')
+        print('image taken')
         data['takeImage'] == None
         json.dump(data,f)
       time.sleep(0.1)
