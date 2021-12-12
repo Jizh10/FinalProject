6a106539dbe588ae0ef7aa580caf244124d7ba6c
James Cobau <jamescobau@gmail.com>
2021-12-12 23:41:15 +0000

    added spaz

---
+++ spaz.py
@@ -0,0 +1,40 @@
+import distance
+import time
+import numpy as np
+import rotational
+import Linear
+import RPi.GPIO as GPIO
+
+sens = distance.ultrasonic(echo = 22, trig = 27)
+lin = Linear.Linear()
+
+stepper = rotational.rot(step = 19, dir = 26)
+
+dists = []
+#average
+for i in range(10):
+  dists = np.append(dists,sens.getDist())
+  time.sleep(0.1)
+
+dist = np.average(dists)
+
+print("dist =")
+print(dist)
+theta0 = 0.001
+x0 = dist*np.sin(theta0)
+y0 = dist*np.cos(theta0)
+try:
+  for i in enumerate(np.concatonate(range(900),range(900,0))):
+    xc = i
+
+    theta = np.arctan((x0-xc)/y0)
+    print("theta {:f}".format(theta))
+    print("xc {:f}".format(xc))
+    print("y0 {:f}".format(y0))
+    stepper.angle(theta)
+    lin.move(xc)
+    
+    time.sleep(0.001)
+except:
+  stepper.angle(0)
+  GPIO.cleanup()
