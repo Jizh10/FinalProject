85fcfe579c883f6579f90ea790fc1bfc209f8a48
James Cobau <jamescobau@gmail.com>
2021-12-13 01:41:43 +0000

    ugh

--- spaz.py
+++ spaz.py
@@ -1,4 +1,3 @@
-import distance
 import time
 import numpy as np
 import rotational
@@ -7,26 +6,12 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 
 stepper = rotational.rot(step = 19, dir = 26)
-speed = input()
-sens = distance.ultrasonic(echo = 22, trig = 27)
+
 lin = Linear.Linear()
 
 lin.move(0)
 
 
-dists = []
-#average
-for i in range(10):
-  dists = np.append(dists,sens.getDist())
-  time.sleep(0.1)
-
-dist = np.average(dists)
-
-print("dist =")
-print(dist)
-theta0 = 0.001
-x0 = dist*np.sin(theta0)
-y0 = dist*np.cos(theta0)
 try:
   while True:
     for i in np.append(range(0,900,50),range(900,0,-50)):
