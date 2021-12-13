19e87ba587c05b8b56ccccb577d2e9b965513f80
jzheng10 <48421695+Jizh10@users.noreply.github.com>
2021-12-13 20:31:54 +0000

    center

--- server.py
+++ server.py
@@ -256,7 +256,7 @@ def main():
                     # calculate the angle
                     theta = np.arctan((x0-xc)/y0)
                     # go to the angle
-                    if i % 4 == 0:
+                    if i % 8 == 0:
                       rotation.angle(theta, speed=20*16*5)
 
                     sleep(5/1000)
